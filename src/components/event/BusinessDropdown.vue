4dc96f0d013fe1a37a61126af1cdd1adda370a93
Madli <madli.petuhhov89@gmail.com>
2024-06-06 11:31:30 +0300

    dropdown default wip

--- src/components/event/BusinessDropdown.vue
+++ src/components/event/BusinessDropdown.vue
@@ -1,6 +1,6 @@
 <template>
   <select v-model="selectedBusinessId" @change="emitSelectedBusinessId" class="form-select">
-    <option selected disabled value="0">Vali etteõte</option>
+<!--    <option selected disabled value="0">Vali ettevõte</option>-->
     <option v-for="business in businesses" :value="business.businessId" :key="business.businessId">
       {{ business.companyName }}
     </option>
@@ -31,6 +31,10 @@ export default {
       this.$http.get('/businesses')
           .then(response => {
             this.businesses = response.data
+            if (this.businesses.length > 0) {
+              this.selectedBusinessId = this.businesses[0].businessId;
+            }
+            this.emitSelectedBusinessId()
           })
           .catch(() => {
             router.push({name: 'errorRoute'})
